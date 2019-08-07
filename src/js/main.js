f928a7b8b1877ab617b317cc66c5f8e13fd7b7da
Paweł Kochowicz <pawel.kochowicz@gmail.com>
2019-08-07 12:43:46 +0200

    feat:add JS

--- src/js/main.js
+++ src/js/main.js
@@ -3,11 +3,11 @@
 // service worker registration - remove if you're not going to use it
 
 if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('serviceworker.js').then(function(registration) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('serviceworker.js').then(function (registration) {
       // Registration was successful
       console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
+    }, function (err) {
       // registration failed :(
       console.log('ServiceWorker registration failed: ', err);
     });
@@ -16,5 +16,17 @@ if ('serviceWorker' in navigator) {
 
 // place your code below
 
+const list = document.querySelector('.list--js');
 
-
+fetch('https://api.github.com/users/pawelkochowicz/repos')
+  .then(resp => resp.json())
+  .then(resp => {
+    const repos = resp;
+    for (const repo of repos) {
+      console.log(`${repo.name} ${repo.html_url}`)
+      list.innerHTML += `<li><a href="${repo.html_url}">${repo.name}</a> <li>`;
+    }
+  })
+  .catch(err => {
+    console.log(err)
+  })
